e5f3b38d1cf544590430e795a9123d621caceae3
Chris Weldon <chris@chrisweldon.net>
2013-04-26 20:51:25 -0500

    Initial project setup.

---
+++ callforit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace callforit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ callforit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="callforit.MvcApplication" Language="C#" %>
