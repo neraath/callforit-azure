a37bc13b9852d704c99c7717c7e361bf55633967
Chris Weldon <chris@chrisweldon.net>
2013-04-27 12:59:22 -0500

    Adding start date to the model.

--- callforit/Models/Conference.cs
+++ callforit/Models/Conference.cs
@@ -1,9 +1,12 @@
-﻿namespace callforit.Models
+﻿using System;
+
+namespace callforit.Models
 {
     public class Conference
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public DateTime StartDate { get; set; }
     }
 }
