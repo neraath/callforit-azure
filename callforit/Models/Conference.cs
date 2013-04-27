4f8f81bf661255b602dd2e49aa3fe83dd6640926
Chris Weldon <chris@chrisweldon.net>
2013-04-26 21:06:12 -0500

    Creating the Conference model and controller. Creating initial index view.

---
+++ callforit/Models/Conference.cs
@@ -0,0 +1,9 @@
+﻿namespace callforit.Models
+{
+    public class Conference
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
